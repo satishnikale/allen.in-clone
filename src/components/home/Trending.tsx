26d3bc9e28cff964c5e6d93d8004804713e3fb98
Satish Nikale <satishnikale7@gmail.com>
2025-06-07 17:57:46 +0530

    Fix image bug in rankers image

--- src/components/home/Trending.tsx
+++ src/components/home/Trending.tsx
@@ -4,6 +4,7 @@ import trending_1 from "../../assets/images/trending-img-1.avif";
 import trending_2 from "../../assets/images/trending-img-2.avif";
 import trending_3 from "../../assets/images/trending-img-3.avif";
 import trending_4 from "../../assets/images/trending-img-4.avif";
+import rankeres_img from "../../assets/images/rankers-img-homepage.avif";
 
 export const Trending = () => {
 
@@ -21,7 +22,7 @@ export const Trending = () => {
             <div className="py-10">
                 <img 
                 className="w-full h-[200px] sm:h-full"
-                src="src/assets/images/rankers-img-homepage.avif" alt="" loading="lazy" />
+                src={rankeres_img} alt="" loading="lazy" />
             </div>
           <div>
             <h1 className="font-poppins text-center text-2xl font-medium py-10">
